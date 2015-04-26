f2ea82c3631084d1f7b49612702f18b7e5aa9b75
unknown <jstola@STOLA-PC.oradev.oraclecorp.com>
2015-04-26 22:39:04 +0200

    Solution for the Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this file calculate and cache
+## the inverse of a matrix. These functions are
+## useful when we need the inverse of some matrix
+## repeatedly and we don't want to perform
+## the (time-consuming) calculation of the inverse
+## every time we need it.
 
-## Write a short comment describing this function
+## This function creates a special wrapper of the given matrix.
+## The returned wrapper-object can be used as an argument
+## to cacheSolve function for cached calculation
+## of the matrix inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL
+        set <- function(y) {
+                x <<- y
+                i <<- NULL
+        }
+        get <- function() x
+        setInverse <- function(inverse) i <<- inverse
+        getInverse <- function() i
+        list(set = set, get = get,
+             setInverse = setInverse,
+             getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the matrix
+## wrapped by the call to makeCacheMatrix above.
+## Do not pass to this function a regular matrix.
+## This function accepts wrapper-objects created
+## by function makeCacheMatrix only.
+## The function calculates the inverse when it
+## was not calculated before (for the given matrix) only.
+## It returns the cached value otherwise.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()
+        if(!is.null(i)) {
+                message("getting cached data")
+                return(i)
+        }
+        data <- x$get()
+        i <- solve(data, ...)
+        x$setInverse(i)
+        i
 }
